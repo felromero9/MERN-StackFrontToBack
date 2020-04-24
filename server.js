0e4dc984c2686cd76636fac015f538d07e6e04f5
felromero9 <felromero9@gmail.com>
2020-04-24 16:19:09 +0200

    add validated user with token api/users/current

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -24,7 +25,13 @@ mongoose
   .then(() => console.log("MongoDB Connected successfully!"))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("hello"));
+//app.get("/", (req, res) => res.send("hello"));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport.js")(passport);
 
 // Use Routes
 app.use("/api/users", users);
