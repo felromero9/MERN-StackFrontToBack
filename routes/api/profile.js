8ced59de3497cb6a753ba7e81b5f7fc3579a9ea1
Felipe Romero <felromero9@gmail.com>
2020-04-30 18:11:13 +0200

    add POST api/profile profile.js, update profile and save

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -38,4 +38,62 @@ router.get(
   }
 );
 
+//@route    POST api/profile
+//@des      Create & edit user profile
+//@access   Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // Get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.webside) profileFields.webside = req.body.webside;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+
+    // Skills - Split into array
+    if (typeof req.body.skills !== "undefined") {
+      profileFields.skills = req.body.skills.split(",");
+    }
+
+    // Social
+    profileFields.social = {};
+    if (req.body.likedin) profileFields.social.skills = req.body.likedin;
+    if (req.body.youtube) profileFields.social.skills = req.body.youtube;
+    if (req.body.facebook) profileFields.social.skills = req.body.facebook;
+    if (req.body.twitter) profileFields.social.skills = req.body.twitter;
+    if (req.body.instagram) profileFields.social.skills = req.body.instagram;
+
+    Profile.findOne({ user: req.user.id }).then((profile) => {
+      if (profile) {
+        // If exists then will UPDATE the profile
+        Profile.findByIdAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then((profile) => res.json(profile));
+      } else {
+        // If not exists will CREATE the profile
+
+        // Check if handle exists
+        Profile.findOne({ handle: profileFields.handle }).then((profile) => {
+          if (profile) {
+            errors.handle = "That handle already exists !";
+            res.status(400).json(errors);
+          }
+
+          // Save Profile
+          new Profile(profileFields)
+            .save()
+            .then((profile) => res.json(profile));
+        });
+      }
+    });
+  }
+);
+
 module.exports = router;
